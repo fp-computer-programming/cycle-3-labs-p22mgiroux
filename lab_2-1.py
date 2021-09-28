1cdc880d7618f82dfab489f96326814ab2f871f2
p22mgiroux <p22mgiroux@fairfieldprep.org>
2021-09-28 09:56:17 -0400

    Submit labs 2-1 & 2-2

---
+++ lab_2-1.py
@@ -0,0 +1,6 @@
+# Author MOG 9/28/21
+
+number = input("Please input a number: ")
+
+if int(number) % 2 == 0:
+    print(number + " is an even number")
---
+++ lab_2-2.py
@@ -0,0 +1,8 @@
+# Author MOG 9/28/21
+
+number = input("Please input a number: ")
+
+if int(number) % 2 == 0:
+    print(number + " is an even number")
+else:
+    print(number + " is an odd number")
