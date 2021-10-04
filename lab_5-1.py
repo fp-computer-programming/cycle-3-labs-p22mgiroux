89fe26b5fd89b2409e5a2fd398b22cfaadb9e3aa
p22mgiroux <p22mgiroux@fairfieldprep.org>
2021-10-04 09:57:57 -0400

    update lab to include error types

--- lab_5-1.py
+++ lab_5-1.py
@@ -1,7 +1,16 @@
 # Author: MOG 10/4/21
 
 # int(a)
+# Name error
+
 # int("a")
+# Value error
+
 # "a" + 2
+# Type Error
+
 # import date
+# Module Not Found error
+
 # print("I'm a happy camper)
+# Syntax error
